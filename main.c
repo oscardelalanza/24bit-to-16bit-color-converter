6ddae368593f5958a147958895c455c675b1d6ca
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-15 07:47:43 -0700

    color converter

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// 128, 255, 114 screaming green
+unsigned int colorConvert(unsigned int color24bits);
+
+int main() {
+  printf("%x", colorConvert(0x80ff72));
+  return 0;
+}
+
+unsigned int colorConvert(unsigned int color24bits) {
+  unsigned int color16bits, red, green, blue;
+
+  red = color24bits >> 19u;
+  green = color24bits << 16u >> 26u;
+  blue = color24bits << 24u >> 27u;
+
+  color16bits = red << 11u | green << 5u | blue;
+
+  return color16bits;
+}
